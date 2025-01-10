eb9a317c95efafc4f90ea3bf7c64d991c75b4efd
Kazenoyama <kishan.turpin@gmail.com>
2025-01-10 14:11:11 +0100

    Change in the package to facilitate the use of it

--- main.js
+++ main.js
@@ -3,22 +3,15 @@ import Framework from '../framework/framework.js';
 import { OrbitControls } from 'three/examples/jsm/Addons.js';
 import Table from '../framework/table.js';
 
-
-
 const fw = new Framework();
 function printaMessage(){
     console.log("Hello World");
 }
 
-// fw.addButtonToNavbar("Hello world", printaMessage);
-// fw.addButtonToNavbar("AAAAAA", printaMessage);
-
-
-
 fw.addButtonToNavbar("Button 1", printaMessage);
 fw.addButtonToNavbar("Button 2", printaMessage);
-fw.addDropdownToNavbar("DropDown 1",[{ text: "Premier boutton", onClick: () => alert("Hello!") },{ text: "Paarrraaa", onClick: () => alert("Hello!") }])
-fw.addDropdownToNavbar("DropDown 2",[{ text: "Parameters", onClick: () => alert("Hellozaeazsd!") },{ text: "P", onClick: () => alert("Hellosdqsdq!") }])
+fw.addDropdownToNavbar("DropDown 1",[{ text: "Button DropDown 1", onClick: printaMessage },{ text: "Button DropDown 2", onClick: () => alert("Hello!") }])
+fw.addDropdownToNavbar("DropDown 2",[{ text: "Button DropDown 3", onClick: printaMessage},{ text: "Button DropDown 4", onClick: () => alert("Hello World!") }])
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -31,39 +24,29 @@ camera.position.y = 50;
 const camera2 = new OrbitControls(camera, renderer.domElement);
 camera2.update();
 
-
 fw.onResize(renderer, window, camera);
-
-
 
 const raycaster = new THREE.Raycaster();
 const direction = new THREE.Vector3(1, 0, 0);
 
-
-////////////////////
-
 const textures = ["src/textures/wood_floor.jpg", "src/textures/wall.jpg", "src/textures/roof.jpg"];
 fw.addScene(scene, textures, fw);
 
- const maxDistance = 1000;
+ const maxDistance = 100;
 
 
 camera2.addEventListener('change', () => {
-    const distance = camera.position.length(); // Distance from (0, 0, 0)
+    const distance = camera.position.length();
     if (distance > maxDistance) {
-        // Restrict the camera's position by scaling it back to maxDistance
         camera.position.normalize().multiplyScalar(maxDistance);
-        controls.update(); // Update controls to reflect new position
+        controls.update();
     }
-
 });
 
 let table = new Table(50,50);
 scene.add(table.getTable());
 
 fw.attachLight(scene, 'white', 1, table.getTable())
-
-////////////////////
 
 function animate() {
     requestAnimationFrame(animate);
