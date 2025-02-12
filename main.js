fa33f944af7d95aafa45295b5589128590097ef6
Kazenoyama <kishan.turpin@gmail.com>
2025-02-12 10:52:58 +0100

    fix main.js for a version that works

--- main.js
+++ main.js
@@ -3,9 +3,7 @@ import Framework from '../../framework/js/framework.js';
 
 const fw = new Framework();
 const scene = fw.mainParameters.scene;
-// const camera = fw.mainParameters.camera;
 const renderer = fw.mainParameters.renderer;
-await fw.addSceneFromJson("src/models/test.json");
 const camera = fw.mainParameters.camera;
 
 function printaMessage(){
@@ -20,15 +18,10 @@ var button2 = fw.addButtonToNavbar("Button 2", printaMessage);
 var button3 = fw.addDropdownToNavbar("DropDown 1",[{ text: "Button DropDown 1", onClick: printaMessage },{ text: "Button DropDown 2", onClick: () => alert("Hello!") }])
 var button4 = fw.addDropdownToNavbar("DropDown 2",[{ text: "Button DropDown 3", onClick: printaMessage},{ text: "Button DropDown 4", onClick: () => alert("Hello World!") }])
 
-// button1.textContent = "Button 1 is changed";
-// button3.children[0].textContent = "DropDown 1 is changed";
-// button3.children[1].children[0].textContent = "Sub section 1 is changed";
-
 const raycaster = new THREE.Raycaster();
 const direction = new THREE.Vector3(1, 0, 0);
 
-// const table = fw.addSimpleSceneWithTable(50, 50);
-// fw.addSimpleSceneWithoutTable();
+fw.addSimpleSceneWithoutTable();
 
 fw.onResize(renderer, window, camera);
 
